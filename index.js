0b976bf9cbc14619caf3376dd1c9e2d162fc4af9
fornesarturo <fornesarturo@gmail.com>
2018-01-29 13:54:42 -0600

    MariaDB connection

--- index.js
+++ index.js
@@ -11,43 +11,53 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var mariadb;
 var apiRouter = express.Router();
 
+app.use(apiRouter);
+
 apiRouter.route("/api/xml")
 .get((req, res, next) => {
     let data;
     let user = req.query["name"] || "default";
-    mariadb.query("SELECT * FROM usuarios WHERE username = :id", 
+    mariadb.query("SELECT * FROM test_santoclos.usuarios WHERE username = :id", 
         {id : user}, (err, rows) => {
             if (err) throw err;
-            console.dir(rows);
+            if (rows.info.numRows > 0) {
+                let data = [];
+                rows.forEach(element => {
+                    data.push(element);
+                });
+                console.log(data);
+                res.set('Content-Type', 'text/xml');
+                res.send(o2x({
+                    '?xml version="1.0" encoding="utf-8"?': null,
+                    data
+                }));
+            }
         });
-    if(data) {
-        res.set('Content-Type', 'text/xml');
-        res.send(o2x({
-            '?xml version="1.0" encoding="utf-8"?': null,
-            data
-        }));
-    }
 });
 
 apiRouter.route("/api/json")
 .get((req, res, next) => {
-    let data;
     let user = req.query["name"] || "default";
-    mariadb.query("SELECT * FROM usuarios WHERE username = :id",
+    mariadb.query("SELECT * FROM test_santoclos.usuarios WHERE username = :id",
         {id: user}, (err, rows) => {
             if (err) throw err;
-            console.dir(rows);
+            if (rows.info.numRows > 0) {
+                let data = [];
+                rows.forEach(element => {
+                    data.push(element);
+                    console.log(element);
+                });
+                res.json(data);
+            }
         });
-    if(data) {
-        res.json(data);
-    }
 })
 
 http.createServer(app).listen(8080, () => {
+    let mariaPassword = process.argv[2];
     mariadb = new MariaDBClient({
         host: 'localhost',
-        user: 'admin',
-        password: 'admin'
+        user: 'root',
+        password: mariaPassword
     });
     console.log(mariadb);
     console.log("Listening on port 8080 . . .");
