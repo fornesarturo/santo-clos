cea2b1ba078614b96393d7732d35d5924998097a
Miguel Montoya <miguel.mzbi@gmail.com>
2018-03-12 16:33:39 -0600

    Log Out

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const util = require('../util/util.js');
 const cache = require('../util/cache.js');
-var mariadb = require('../util/mariadb.js');
+const mariadb = require('../util/mariadb.js');
 const auth = require('../util/authenticate.js');
 const path = require('path');
 
@@ -15,6 +15,13 @@ staticRouter.route('/')
 staticRouter.route('/main')
     .get(cache(20), auth.authenticateJWT, (req, res, next) => {
         res.status(200).sendFile(path.join(__dirname, '../html', 'main.html'));
+    });
+
+staticRouter.route('/logout')
+    .get(cache(20), (req, res, next) => {
+        res.clearCookie("token");
+        res.clearCookie("current_user");
+        res.status(200).sendFile(path.join(__dirname, '../html', 'index.html'));
     });
 
 module.exports = staticRouter;
