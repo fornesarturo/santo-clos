d6c8df2dc5a1163518c199d0436b25dfffa4829e
fornesarturo <fornesarturo@gmail.com>
2018-04-29 00:08:08 -0500

    Now with docker

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
+const path = require('path')
 const cookieParser = require('cookie-parser');
 const logger = require('./util/logger.js');
 const dotenv = require('dotenv').config();
@@ -14,17 +15,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(logger);
 
-const cors = require('cors');    
-app.use(cors({credentials: true, origin: ['http://localhost:8081', 'http://localhost:8080', 'http://localhost:' + PORT]}));
+//const cors = require('cors');    
+//app.use(cors({credentials: true, origin: ['http://localhost:8081', 'http://localhost:8080', 'http://localhost:' + PORT]}));
 // app.use(cors({credentials: true, origin: '*'}));
+
+app.use(express.static(__dirname + '/html'));
+app.use(express.static(path.join(__dirname, 'santo-clos-front', 'static')))
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use('/', require('./routes/staticRouter.js'));
 app.use('/auth', require('./routes/authRouter.js'));
 app.use('/api', require('./routes/apiRouter.js'));
 // app.use('/api/xml', require('./routes/xmlRouter.js'));
 
-app.use(express.static( __dirname + '/html'));
-
 http.createServer(app).listen(PORT, () => {
-    console.log("Listening on port 8080 . . .\n");
+    console.log(`Listening on port ${PORT} . . .\n`);
 });
