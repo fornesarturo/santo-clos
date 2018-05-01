f3880ff17736b0f796ca61d2323c16aaa87de356
fornesarturo <fornesarturo@gmail.com>
2018-05-01 13:19:21 -0500

    Removed unused routes

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -16,35 +16,4 @@ staticRouter.route('/')
         res.status(200).sendFile(path.join(__dirname, '../santo-clos-front', "dist", 'index.html'));
     });
 
-staticRouter.route('/main')
-    .get(cache(20), auth.authenticateJWT, (req, res, next) => {
-        if(req.cookies.tokenEvent) {
-            let tokenEvent = req.cookies.tokenEvent;
-            res.clearCookie("tokenEvent");
-            auth.authenticateJWTInvite(tokenEvent).then((event) => {
-                let id = event.eventId;
-                let email = event.email;
-                let user = req.cookies.current_user;
-                mariadb.query("SELECT * FROM user WHERE username = :username", { username: user }, 
-                (err, rows) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                    else {
-                        let emailDB = util.process(rows)[0].email
-                        if(email == emailDB) util.addUserToEvent(user, id, null);
-                    }
-                });
-            });
-        }
-        res.status(200).sendFile(path.join(__dirname, '../html', 'main.html'));
-    });
-
-staticRouter.route('/logout')
-    .get(cache(20), (req, res, next) => {
-        res.clearCookie("token");
-        res.clearCookie("current_user");
-        res.status(200).sendFile(path.join(__dirname, '../html', 'index.html'));
-    });
-
 module.exports = staticRouter;
