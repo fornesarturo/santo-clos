e3f38dc9d11e01533e7be4337f2d39ea1d972f52
Miguel Montoya <miguel.mzbi@gmail.com>
2018-05-01 23:21:42 -0500

    build

--- santo-clos-front/src/components/JoinedEvent.vue
+++ santo-clos-front/src/components/JoinedEvent.vue
@@ -43,7 +43,6 @@ export default {
                             }else if(this.sortDone){
                                 this.$emit("update:sortDone", true);
                             }
-                            console.log(this.sortDone);
                             request.getWishlist(this.id, myGifteeUsername).then(
                                 (resGifteeWishlist) => {
                                     let data = { 
